a800fbaf29269e04b2266baeee6c47e3492f4005
AhmetCan1998 <ahmetcansaptir@gmail.com>
2020-12-28 20:15:07 +0300

    update config.json

--- config.py
+++ config.py
@@ -7,3 +7,11 @@ class Configuration(metaclass=Singleton):
     def __init__(self, name):
         with open(name, "r") as file:
             self.config = json.load(file)
+
+    "database": {
+        "host" : "ahmetpostgres1966.postgres.database.azure.com",
+        "dbname" : "postgres",
+        "user" : "ahmet1998",
+        "password" : "Balkes.1998",
+        "ssl" : "require"
+    }
